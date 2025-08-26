bac5c41c305c19383c49ef4bd154fdcf4693a520
Alial-jirrbi99 <ajirrbi@gmail.com>
2025-08-26 14:00:08 +0300

    change text color in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,11 @@ export default function HomePage() {
   return (
     <div className="container mx-auto px-4 py-8">
       {/* Welcome Section */}
-      <section className="text-center bg-blue-100 p-8 rounded-lg mb-12 shadow-md">
-        <h1 className="text-5xl font-extrabold text-gray-800 mb-2">
+      <section className="text-center bg-gray-200 p-8 rounded-lg mb-12 shadow-md">
+        <h1 className="text-5xl font-extrabold text-blue-300 mb-2">
           Welcome to the Amana Bookstore!
         </h1>
-        <p className="text-lg py-4 text-gray-600">
+        <p className="text-base py-4 text-gray-500">
           Your one-stop shop for the best books. Discover new worlds and
           adventures.
         </p>
